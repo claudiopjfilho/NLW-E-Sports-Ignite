89954fa3f92bbce6a08a3345a0a6641dd1aa3c7f
Claudio Filho <54292133+claudiopjfilho@users.noreply.github.com>
2022-11-07 20:02:32 -0300

    add components

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,23 @@
 import { MagnifyingGlassPlus } from "phosphor-react";
+import { GameBanner } from "./Components/GameBanner";
+
 
 import logoImg from "./assets/Logo-esports-ignite.svg";
+import CreateAdBanner from "./Components/CreateAdBanner";
+import { useState } from "react";
 
 function App() {
+  
+const [hasUserClickedOnButton, setHasUserClickedOnButton] = useState(false)
+
+function handleButtonCLick() {
+  setHasUserClickedOnButton(true)
+}
+
+
+
   return (
-    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+    <div className="xl:max-w-3xl 2xl:max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img src={logoImg} alt="" />
 
       <h1 className="text-6xl text-white  font-black mt-20">
@@ -15,86 +28,16 @@ function App() {
         está aqui.
       </h1>
 
-      <div className="grid grid-cols-6 gap-6 mt-16">
-        <a href="" className="relative rounded-lg overflow-hidden">
-          <img src="/image1.png" alt="" />
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute left-0 bottom-0 right-0">
-            <strong className="font-bold text-white block">
-              League Of Legends
-            </strong>
+<button onClick={handleButtonCLick}>
 
-            <span className="text-zinc-300 text-sm block mt-1">4 Anúncios</span>
-          </div>
-        </a>
-        <a href="" className="relative rounded-lg overflow-hidden">
-          <img src="/image2.png" alt="" />
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute left-0 bottom-0 right-0">
-            <strong className="font-bold text-white block">
-              League Of Legends
-            </strong>
+  Clique Aqui
+</button>
 
-            <span className="text-zinc-300 text-sm block mt-1">4 Anúncios</span>
-          </div>
-        </a>
-        <a href="" className="relative rounded-lg overflow-hidden">
-          <img src="/image3.png" alt="" />
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute left-0 bottom-0 right-0">
-            <strong className="font-bold text-white block">
-              League Of Legends
-            </strong>
+{hasUserClickedOnButton ? 'Usuario clicou' : null}
 
-            <span className="text-zinc-300 text-sm block mt-1">4 Anúncios</span>
-          </div>
-        </a>
-        <a href="" className="relative rounded-lg overflow-hidden">
-          <img src="/image5.png" alt="" />
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute left-0 bottom-0 right-0">
-            <strong className="font-bold text-white block">
-              League Of Legends
-            </strong>
+      <GameBanner adsCount={4} bannerUrl={"../public/image1.png"} title={"LOL"} />
+      <CreateAdBanner/>
 
-            <span className="text-zinc-300 text-sm block mt-1">4 Anúncios</span>
-          </div>
-        </a>
-        <a href="" className="relative rounded-lg overflow-hidden">
-          <img src="/image6.png" alt="" />
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute left-0 bottom-0 right-0">
-            <strong className="font-bold text-white block">
-              League Of Legends
-            </strong>
-
-            <span className="text-zinc-300 text-sm block mt-1">4 Anúncios</span>
-          </div>
-        </a>
-        <a href="" className="relative rounded-lg overflow-hidden">
-          <img src="/image7.png" alt="" />
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute left-0 bottom-0 right-0">
-            <strong className="font-bold text-white block">
-              League Of Legends
-            </strong>
-
-            <span className="text-zinc-300 text-sm block mt-1">4 Anúncios</span>
-          </div>
-        </a>
-      </div>
-
-      <div className="pt-1 mt-8 bg-nlw-gradient self-stretch rounded-lg overflow-hidden">
-        <div className=" bg-[#2a2634] px-8 py-6 flex justify-between items-center  ">
-          <div>
-            <strong className="text-2xl text-white font-black block">
-              Não encontrou seu duo?
-            </strong>
-            <span className="text-zinc-400 block">
-              Publique um anúncio para encontrar novos players!
-            </span>
-          </div>
-
-          <button className="py-3 px-4  bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
-            <MagnifyingGlassPlus size={24} />
-            Publicar anúncio
-          </button>
-        </div>
-      </div>
     </div>
   );
 }
---
+++ web/src/Components/GameBanner.tsx
@@ -0,0 +1,28 @@
+interface GameBannerProps {
+
+  bannerUrl: string;
+  title: string;
+  adsCount: number;
+
+}
+
+
+
+export  function GameBanner(props: GameBannerProps) {
+  return (
+    <div className="grid grid-cols-6 gap-6 mt-16">
+        <a href="" className="relative rounded-lg overflow-hidden">
+    <img src={props.bannerUrl} alt="" />
+    <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute left-0 bottom-0 right-0">
+      <strong className="font-bold text-white block">
+        {props.title}
+      </strong>
+
+      <span className="text-zinc-300 text-sm block mt-1">
+        {props.adsCount} anúncio(s)
+        </span>
+    </div>
+  </a>
+  </div>
+  )
+}
